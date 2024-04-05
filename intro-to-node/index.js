3e2ecaf87b237d44fa86f006410bc75293be8a95
justaddisu <justaddisu@gmail.com>
2024-04-05 08:45:10 +0300

    get superhero name

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,5 +1,11 @@
 const fs = require("fs");
+const superheroes = require("superheroes");
+const supervillains = require('supervillains');
+
 
 fs.copyFileSync("text.txt", "text2.txt");
 
-console.log("text.tet contents are copy to text2.txt");
+console.log(superheroes.random());
+console.log(supervillains.random());
+
+console.log("Hello addisu, I am working ...");
